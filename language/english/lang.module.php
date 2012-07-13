aed820b611bcb9021e1fa2de95fb3222c855f2d4
Bryant Hughes <bryant@thegoodlab.com>
2012-07-13 17:20:40 -0500

    adding module starter files

---
+++ language/english/lang.module.php
@@ -0,0 +1,22 @@
+<?php
+$lang = array(
+
+// Required for MODULES page
+'module_module_name' => 'Module',
+'module_module_description' => 'Brief description of module',
+
+//----------------------------------------
+
+// Additional Key => Value pairs go here
+
+'module_nav_home' => 'Home',
+'module_nav_settings' => 'Settings',
+
+'module_page_title_index' => 'Home',
+'module_page_title_settings' => 'Settings',
+
+// END
+'' => ''
+);
+
+/* End of File: lang.module.php */
